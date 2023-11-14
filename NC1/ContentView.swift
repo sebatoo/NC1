7fd617c51c7fe44aa926e5885050f4cc9d013536
Sebastian Leon <seleco23@gmail.com>
2023-11-14 16:17:18 +0100

    Initial Commit

---
+++ NC1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC1
+//
+//  Created by Gustavo Sebastian Leon Cortez on 14/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC1/NC1App.swift
@@ -0,0 +1,17 @@
+//
+//  NC1App.swift
+//  NC1
+//
+//  Created by Gustavo Sebastian Leon Cortez on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NC1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
