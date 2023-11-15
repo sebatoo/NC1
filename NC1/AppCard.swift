a660a366dc85468b8f4259381551f304626d1272
Sebastian Leon <seleco23@gmail.com>
2023-11-15 17:51:02 +0100

    Created App Card view. Still missing headline text overlay.

--- NC1/AppCard.swift
+++ NC1/AppCard.swift
@@ -9,24 +9,69 @@ import SwiftUI
 
 struct AppCard: View {
     
-    let app: AppModel
+    //let app: AppModel
+    let app: AppModel = AppModel(name: "Duolingo", icon: "duolingoIcon", description: "The language learning app", image: "duolingoImage", banner: "Now available")
     
     var body: some View {
         ZStack {
-//            Color(.blue)
-//                .frame(width: 300)
-            Image(app.image)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-//                .frame(width: 350, height: 450)
-                .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/))
-                .padding()
-                .background(Color(.blue))
-            Text(app.name)
+            Color(.gray)
+                .frame(width: 350, height: 430)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            VStack {
+                ZStack {
+                    Image(app.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 350, height: 350)
+                        .clipShape(RoundedRectangle(cornerRadius: 15))
+                        .mask(LinearGradient(gradient: Gradient(colors: [Color.white, Color.white, Color.white.opacity(0)]), startPoint: .top, endPoint: .bottom))
+                }
+                ZStack {
+                    Color(.black)
+                        .opacity(0.2)
+                        .frame(height: 73)
+                        .clipShape(UnevenRoundedRectangle(cornerRadii: .init(bottomLeading: 15,bottomTrailing: 15)))
+                    HStack {
+                        Spacer()
+                        Image(app.icon)
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 48, height: 48)
+                            .clipShape(RoundedRectangle(cornerRadius: 10
+                                                       ))
+                        VStack(alignment: .leading) {
+                            Text(app.name)
+                                .bold()
+                                .font(.subheadline)
+                            Text(app.description)
+                                .font(.footnote)
+                                .opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                        }
+                        .foregroundStyle(.white)
+                        Spacer()
+                        VStack {
+                            Button("Get") {}
+                                .frame(minWidth: 70, minHeight: 30)
+                                .background(Capsule().fill(Color.secondary))
+                                //.buttonStyle(.bordered)
+                                .fontWeight(.bold)
+                                //.clipShape(Capsule())
+                            Text("In-App Purchases")
+                                .font(.custom("small", size: 9))
+                                .opacity(0.8)
+                        }
+                        .foregroundStyle(.white)
+                        .lineLimit(1)
+                        Spacer()
+                    }
+                }
+                .frame(width: 350, height: 80)
+            }
         }
     }
 }
 
 #Preview {
-    AppCard(app: AppModel(name: "Duolingo", icon: "duolingoIcon", description: "The language learning app.", image: "duolingoImage", banner: "Now available"))
+    //AppCard(app: AppModel(name: "Duolingo", icon: "duolingoIcon", description: "The language learning app", image: "duolingoImage", banner: "Now available"))
+    AppCard()
 }
--- NC1/NC1App.swift
+++ NC1/NC1App.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct NC1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            AppCard()
         }
     }
 }
